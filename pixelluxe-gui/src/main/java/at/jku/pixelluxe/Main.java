3ef3c62720ad47fb3823e8c476293269bbedccf2
VanDulti <me@ulrich-dultinger.dev>
2024-04-04 21:38:08 +0200

    feat: initial project setup

---
+++ pixelluxe-gui/src/main/java/at/jku/pixelluxe/Main.java
@@ -0,0 +1,17 @@
+package at.jku.pixelluxe;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        MainForm mainForm = new MainForm();
+        mainForm.initialize();
+        JPanel mainPanel = mainForm.getMainPanel();
+
+        JFrame mainFrame = new JFrame("PixelLuxe");
+        mainFrame.setContentPane(mainPanel);
+        mainFrame.setResizable(true);
+        mainFrame.setSize(200, 100);
+        mainFrame.setVisible(true);
+    }
+}
---
+++ pixelluxe-gui/src/main/java/at/jku/pixelluxe/MainForm.java
@@ -0,0 +1,19 @@
+package at.jku.pixelluxe;
+
+import javax.swing.*;
+
+public class MainForm {
+    private JPanel mainPanel;
+
+    public JPanel getMainPanel() {
+        return mainPanel;
+    }
+
+    public void initialize() {
+        mainPanel = new JPanel();
+        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+
+        JLabel label = new JLabel("Hello World!");
+        mainPanel.add(label);
+    }
+}
