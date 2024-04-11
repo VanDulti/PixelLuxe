6aeb06bf6ddbedec8a649ea126dc2036431e5fe9
VanDulti <me@ulrich-dultinger.dev>
2024-04-11 16:00:52 +0200

    asdfasdfasdfasdf

--- pixelluxe-gui/src/main/java/at/jku/pixelluxe/MainForm.java
+++ pixelluxe-gui/src/main/java/at/jku/pixelluxe/MainForm.java
@@ -3,17 +3,17 @@ package at.jku.pixelluxe;
 import javax.swing.*;
 
 public class MainForm {
-    private JPanel mainPanel;
+	private JPanel mainPanel;
 
-    public JPanel getMainPanel() {
-        return mainPanel;
-    }
+	public JPanel getMainPanel() {
+		return mainPanel;
+	}
 
-    public void initialize() {
-        mainPanel = new JPanel();
-        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+	public void initialize() {
+		mainPanel = new JPanel();
+		mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
 
-        JLabel label = new JLabel("Hello World!");
-        mainPanel.add(label);
-    }
+		JLabel label = new JLabel("Hello World!");
+		mainPanel.add(label);
+	}
 }
