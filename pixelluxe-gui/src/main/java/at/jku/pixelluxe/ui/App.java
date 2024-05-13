45d029792a9da4f85741f9ae54219725d60c96a4
VanDulti <me@ulrich-dultinger.dev>
2024-05-13 15:41:06 +0200

    ci: try setup web

--- pixelluxe-gui/src/main/java/at/jku/pixelluxe/ui/App.java
+++ pixelluxe-gui/src/main/java/at/jku/pixelluxe/ui/App.java
@@ -60,11 +60,6 @@ public class App {
 	}
 
 	public static void main(String[] args) {
-		try {
-			UIManager.setLookAndFeel(new FlatDarkLaf());
-		} catch (UnsupportedLookAndFeelException e) {
-			Logger.getGlobal().warning("Failed to set look and feel");
-		}
 		new App().run();
 	}
 
