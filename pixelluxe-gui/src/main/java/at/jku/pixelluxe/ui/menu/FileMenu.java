dabdc1abf6900cec8b759e27dfb7ff40021edddb
VanDulti <me@ulrich-dultinger.dev>
2024-06-01 17:44:31 +0200

    feat: shortcuts + margin

--- pixelluxe-gui/src/main/java/at/jku/pixelluxe/ui/menu/FileMenu.java
+++ pixelluxe-gui/src/main/java/at/jku/pixelluxe/ui/menu/FileMenu.java
@@ -36,11 +36,13 @@ public class FileMenu extends JMenu implements Component<JMenu> {
 		openButton.addActionListener(action -> onOpen.run());
 		openButton.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, InputEvent.CTRL_DOWN_MASK));
 
-		JMenuItem undoButton = new JMenuItem("Undo");
+		JMenuItem undoButton = new JMenuItem("Undo", KeyEvent.VK_Z + InputEvent.CTRL_DOWN_MASK);
 		undoButton.addActionListener(action -> onUndo.run());
+		undoButton.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Z, InputEvent.CTRL_DOWN_MASK));
 
-		JMenuItem redoButton = new JMenuItem("Redo");
+		JMenuItem redoButton = new JMenuItem("Redo", KeyEvent.VK_Y + InputEvent.CTRL_DOWN_MASK);
 		redoButton.addActionListener(action -> onRedo.run());
+		redoButton.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Y, InputEvent.CTRL_DOWN_MASK));
 
 		add(openButton);
 		add(saveButton);
